9abb3ed491a1a34b290f2d5730fe05411728e58d
itmuch <eacdy0000@126.com>
2019-04-18 21:51:16 +0800

    1. 升级jjwt到0.10.5 2. JwtOperator做相应调整，并删除无用代码、优化相关注释

--- light-security-spring-boot-starter/src/main/java/com/itmuch/lightsecurity/jwt/JwtOperator.java
+++ light-security-spring-boot-starter/src/main/java/com/itmuch/lightsecurity/jwt/JwtOperator.java
@@ -3,9 +3,12 @@ package com.itmuch.lightsecurity.jwt;
 import com.itmuch.lightsecurity.autoconfigure.lightsecurity.LightSecurityProperties;
 import com.itmuch.lightsecurity.exception.LightSecurityException;
 import io.jsonwebtoken.*;
+import io.jsonwebtoken.security.Keys;
+import io.jsonwebtoken.security.SignatureException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
+import javax.crypto.SecretKey;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
@@ -15,6 +18,7 @@ import java.util.Map;
  */
 @Slf4j
 @RequiredArgsConstructor
+@SuppressWarnings("WeakerAccess")
 public class JwtOperator {
     public static final String USER_ID = "id";
     public static final String USERNAME = "username";
@@ -22,20 +26,16 @@ public class JwtOperator {
     private final LightSecurityProperties lightSecurityProperties;
 
     public Claims getClaimsFromToken(String token) {
-        Claims claims;
         try {
             return Jwts.parser()
-                    .setSigningKey(lightSecurityProperties.getJwt().getSecret())
+                    .setSigningKey(this.lightSecurityProperties.getJwt().getSecret().getBytes())
                     .parseClaimsJws(token)
                     .getBody();
-        } catch (ExpiredJwtException | UnsupportedJwtException | SignatureException | MalformedJwtException | IllegalArgumentException e) {
+
+        } catch (ExpiredJwtException | UnsupportedJwtException | MalformedJwtException | SignatureException | IllegalArgumentException e) {
             log.error("token解析错误", e);
             throw new LightSecurityException("Token invalided.", e);
         }
-    }
-
-    public String getUsernameFromToken(String token) {
-        return getClaimsFromToken(token).getSubject();
     }
 
     public Date getExpirationDateFromToken(String token) {
@@ -47,33 +47,30 @@ public class JwtOperator {
         return expiration.before(new Date());
     }
 
-//    public String generateToken(String username, Set<String> roles) {
-//        Map<String, Object> claims = new HashMap<>(1);
-//        claims.put("roles", roles);
-//        return this.doGenerateToken(claims);
-//    }
-
     public Date getExpirationTime() {
         return new Date(System.currentTimeMillis() + lightSecurityProperties.getJwt().getExpirationInSecond() * 1000);
     }
 
     public String generateToken(User user) {
-        Map<String, Object> claims = new HashMap<>();
+        Map<String, Object> claims = new HashMap<>(3);
         claims.put(USER_ID, user.getId());
         claims.put(USERNAME, user.getUsername());
         claims.put(ROLES, user.getRoles());
         Date createdTime = new Date();
         Date expirationTime = this.getExpirationTime();
+
+        byte[] keyBytes = this.lightSecurityProperties.getJwt().getSecret().getBytes();
+        SecretKey key = Keys.hmacShaKeyFor(keyBytes);
+
         return Jwts.builder()
                 .setClaims(claims)
                 .setIssuedAt(createdTime)
                 .setExpiration(expirationTime)
-                .signWith(lightSecurityProperties.getJwt().getAlgorithm(), lightSecurityProperties.getJwt().getSecret())
+                .signWith(key)
                 .compact();
     }
 
     public Boolean validateToken(String token) {
         return !isTokenExpired(token);
     }
-
 }
