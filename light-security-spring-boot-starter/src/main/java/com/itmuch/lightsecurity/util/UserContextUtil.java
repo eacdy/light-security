6d8984f3b0613d80b6b87f8aa00391e27b09d5bf
eacdy <eacdy0000@126.com>
2022-03-29 00:31:36 +0800

    1.2.0

--- light-security-spring-boot-starter/src/main/java/com/itmuch/lightsecurity/util/UserContextUtil.java
+++ light-security-spring-boot-starter/src/main/java/com/itmuch/lightsecurity/util/UserContextUtil.java
@@ -5,14 +5,24 @@ import com.itmuch.lightsecurity.jwt.LoginUser;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.Optional;
-
+/**
+ * @author itmuch.com
+ */
 public class UserContextUtil {
+    /**
+     * 获取当前登录用户信息
+     * @return 当前登录用户信息
+     */
     public static LoginUser getUser() {
         HttpServletRequest request = WebUtil.getRequest();
         Object user = request.getAttribute(ConstantsSecurity.LIGHT_SECURITY_REQ_ATTR_USER);
         return (LoginUser) user;
     }
 
+    /**
+     * 获取当前登录用户的id
+     * @return 当前登录用户id
+     */
     public static Integer getUserId() {
         LoginUser loginUser = getUser();
         return Optional.ofNullable(loginUser)
--- light-security-spring-boot-starter/src/main/java/com/itmuch/lightsecurity/util/WebUtil.java
+++ light-security-spring-boot-starter/src/main/java/com/itmuch/lightsecurity/util/WebUtil.java
@@ -11,14 +11,24 @@ import java.util.Arrays;
 import java.util.Objects;
 import java.util.Optional;
 
+/**
+ * @author itmuch.com
+ */
 public class WebUtil {
+    /**
+     * 根据名称过滤出名称匹配的cookie
+     *
+     * @param cookies    cookie列表
+     * @param cookieName cookieName
+     * @return 对应名称的cookie
+     */
     public static Optional<Cookie> filterCookieByName(Cookie[] cookies, String cookieName) {
-        if(cookies == null || cookies.length == 0) {
+        if (cookies == null || cookies.length == 0) {
             return Optional.empty();
         }
 
         return Arrays.stream(cookies)
-                .filter(c-> Objects.equals(c.getName(), cookieName))
+                .filter(c -> Objects.equals(c.getName(), cookieName))
                 .findFirst();
     }
 
